befc8621b8f134941649cdf2f616eeac91056f03
ilja.bobkevic <ilja.bobkevic@klarna.com>
2013-09-05 01:25:56 +0200

    Add db role

---
+++ roles/db.rb
@@ -0,0 +1,13 @@
+name "web"
+description "The base role for mysql database systems"
+run_list "role[base]",
+         "recipe[mysql::server]"
+
+default_attributes({
+  "mysql" => {
+    "bind_address" => "127.0.0.1",
+    "server_debian_password" => "xS5cFQu4",
+    "server_root_password" => "NpaVKuQL",
+    "server_repl_password" => "SxfFC4bJ"
+  }
+})
