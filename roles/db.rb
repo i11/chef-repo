a2e207227309715c2095b771534f2412635dbb3d
ilja.bobkevic <ilja.bobkevic@klarna.com>
2013-09-05 13:01:20 +0200

    Change mysql bind_address to 0.0.0.0

--- roles/db.rb
+++ roles/db.rb
@@ -5,7 +5,7 @@ run_list "role[base]",
 
 default_attributes({
   "mysql" => {
-    "bind_address" => "127.0.0.1",
+    "bind_address" => "0.0.0.0",
     "server_debian_password" => "xS5cFQu4",
     "server_root_password" => "NpaVKuQL",
     "server_repl_password" => "SxfFC4bJ"
